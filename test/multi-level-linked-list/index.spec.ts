1073bd045da9bd57fa3ce483074ecd820d5b0d62
valerachuk <valerachuk@gmail.com>
2022-12-27 17:41:11 +0200

    wip

--- test/multi-level-linked-list/index.spec.ts
+++ test/multi-level-linked-list/index.spec.ts
@@ -5,11 +5,9 @@ import {
   getNodeAt,
 } from "../../src/multi-level-linked-list/utils";
 
+// 1 to 22
 const getTestMultiLevelLinkedList1Flat = () =>
-  [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22,
-  ].concat();
+  [...new Array(22).keys()].map((x) => x + 1);
 
 describe("Multi level linked list", () => {
   let testMultiLevelLinkedList1: MultiLevelLinkedList<number> = null!;
